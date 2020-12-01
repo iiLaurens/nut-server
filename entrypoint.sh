1371f2e0555502144b96333d12af05e727fc78c7
Marco Melletti <mellotanica@hotmail.it>
2020-12-01 21:53:22 +0100

    added flag to enable/disable automatic scraping

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,18 @@
 
 # add custom id and password to secure nut-server
 echo "\n$ID|$PASSWORD" >> /root/nut.src.latest/conf/users.conf
-echo "* * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan >> /var/log/cron.log\n" > /etc/cron.d/scraping-nut
-chmod 0644 /etc/cron.d/scraping-nut && crontab /etc/cron.d/scraping-nut
 
+# link /games to local folder to let nutfs browse folder
 ln -sf /games /root/nut.src.latest/games
 
-#cron -f &
-service cron start
+if $SCRAPING_ENABLED; then
+    echo "* * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan >> /var/log/cron.log\n" > /etc/cron.d/scraping-nut
+    chmod 0644 /etc/cron.d/scraping-nut && crontab /etc/cron.d/scraping-nut
+
+    #cron -f &
+    service cron start
+else
+    crontab /etc/cron.d/no-scraping
+fi
 
 python3 /root/nut.src.latest/nut.py -S >> /var/log/nut.log
