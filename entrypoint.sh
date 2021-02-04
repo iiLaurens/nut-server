8a9b16a34e5ac9a2a7fff4419471cc3ad594565e
iiLaurens <iiLaurens@users.noreply.github.com>
2021-02-04 22:52:16 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,11 @@ echo "\n$ID|$PASSWORD" >> /root/nut-master/conf/users.conf
 ln -sf /games /root/nut-master/games
 
 if $SCRAPING_ENABLED; then
-    echo "* * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan >> /var/log/cron.log\n" > /etc/cron.d/scraping-nut
+    echo "0 * * * * wget --spider --user $ID --password $PASSWORD http://127.0.0.1:9000/api/scan >> /var/log/cron.log\n" > /etc/cron.d/scraping-nut
     chmod 0644 /etc/cron.d/scraping-nut && crontab /etc/cron.d/scraping-nut
 
     #cron -f &
     service cron start
 fi
 
-python3 /root/nut-master/nut.py -S >> /var/log/nut.log
+python3 /root/nut-master/nut.py -S
